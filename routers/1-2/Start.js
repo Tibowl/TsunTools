b2cd9334e93043f63697ba75c9604d15d8b5d355
Tibo442 <datwiliknietzeggen+git@gmail.com>
2018-10-11 22:43:58 +0200

    Add 1-1/A router
    Add 1-2/Start test router

---
+++ routers/1-2/Start.js
@@ -0,0 +1,24 @@
+exports.map = "1-2";
+exports.node = "Start";
+exports.nodesToIgnore = [];
+exports.nodeColors = {
+    'A': '\x1b[42m\x1b[30m', // Green BG
+    'B': '\x1b[41m\x1b[37m', // Red BG
+};
+
+exports.getType = (entry, edgeNames) => {
+    let edge = edgeNames[1];
+    let s = getStypeCount(entry).all;
+    let sc = getSpecialCombines(s);
+
+
+    let type = "other";
+	
+	//return s.CA + s.CAV + sc.aBB + sc.aCV
+
+	//if(entry.fleet1.length != 6) return "6 ships"
+
+    if(type == "other") advancedStypeCounter(entry, Object.assign({}, s, sc, {'aShip': entry.fleet1.length}), edge)
+	
+    return type; 
+}
